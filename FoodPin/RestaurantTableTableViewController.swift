1ef1b485d5900d42f0b258f2ac7b3f3626685ae9
Rodrigo Carballo <rodrigo.puravida@protonmail.com>
2021-05-02 19:02:37 -0700

    Added Location and Type

--- FoodPin/RestaurantTableTableViewController.swift
+++ FoodPin/RestaurantTableTableViewController.swift
@@ -17,6 +17,11 @@ class RestaurantTableTableViewController: UITableViewController {
     var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Chocolate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats And Deli", "Waffle & Wolf", "Five Leaves", "Cafe Lore","Confessional", "Barrafina", "Donostia", "Royal Oak", "CASK Pub and Kitchen"]
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney","New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee &Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+
 
     
     override func viewDidLoad() {
@@ -27,7 +32,7 @@ class RestaurantTableTableViewController: UITableViewController {
         var snapShot = NSDiffableDataSourceSnapshot<Section, String>()
         snapShot.appendSections([.all])
         snapShot.appendItems(restaurantNames, toSection: .all)
-        
+                
         dataSource.apply(snapShot, animatingDifferences: false)
         
     }
@@ -39,6 +44,8 @@ class RestaurantTableTableViewController: UITableViewController {
             cellProvider: {  tableView, indexPath, restaurantName in
                 let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! RestaurantTableViewCell
                 cell.nameLabel.text = restaurantName
+                cell.locationLabel.text = self.restaurantLocations[indexPath.row]
+                cell.typeLabel.text = self.restaurantTypes[indexPath.row]
                 cell.thumbnailImageView.image = UIImage(named: self.restaurantImages[indexPath.row])
                 return cell }
         )
